978f7a8a9293af7318597ec8cec395019443752a
Vipul Chodankar <vipul72c@gmail.com>
2020-02-10 22:27:46 +0530

    Initial setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="footer mt-auto py-3 bg-dark text-white">
+          <div className="container">Footer Content...</div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand href="#home">Excel Marketing Services</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#work">Work</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#link">Contact Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
